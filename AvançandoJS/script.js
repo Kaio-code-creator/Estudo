f7c3f40219e26d718e765534edd40b3d720a851c
Kaio-code-creator <kaioasousa12@gmail.com>
2023-06-10 14:18:04 -0300

    .

--- AvançandoJS/script.js
+++ AvançandoJS/script.js
@@ -2,7 +2,26 @@ let num = document.querySelector('input#fnum')
 let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inlista(n, 1) {
+    if(lista.indexOf (Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
 
 function adicionar() {
-    if(isnumero(num.value) && )
+    if(isNumero(num.value) && !inLista(num.value)) {
+        
+} else {
+    window.alert('Valor inválido ou já encontrado na lista.')
+    }
 }
