5a255492282e180629f00360a492bb1ad3dec0cb
Kaio-code-creator <kaioasousa12@gmail.com>
2023-06-13 15:43:31 -0300

    .

--- AvançandoJS/script.js
+++ AvançandoJS/script.js
@@ -2,6 +2,7 @@ let num = document.querySelector('input#fnum')
 let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
+
 function isNumero(n) {
     if(Number(n) >= 1 && Number(n) <= 100) {
         return true
@@ -10,8 +11,8 @@ function isNumero(n) {
     }
 }
 
-function inlista(n, 1) {
-    if(lista.indexOf (Number(n)) != -1) {
+function inLista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
         return true
     } else {
         return false
